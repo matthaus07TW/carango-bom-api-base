20d8d854b995c5e158fc932cb7bc2b118113ba3a
Renan Luiz <renan.luiz@thoughtworks.com>
2021-07-12 14:38:09 -0300

    Add Swagger in project

--- src/main/java/br/com/caelum/carangobom/CarangoBomApiApplication.java
+++ src/main/java/br/com/caelum/carangobom/CarangoBomApiApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 @EnableSpringDataWebSupport
 public class CarangoBomApiApplication {
 
