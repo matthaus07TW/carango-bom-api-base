de93c33b323458932f3e0d27b6b6ed21ac0d35f6
Renan Luiz <renan.luiz@thoughtworks.com>
2021-07-20 20:51:50 -0300

    Update VehicleDto and implements Dashboard feature

---
+++ src/main/java/br/com/caelum/carangobom/interfaces/Dashboard.java
@@ -0,0 +1,13 @@
+package br.com.caelum.carangobom.interfaces;
+
+import java.math.BigDecimal;
+
+public interface Dashboard {
+
+	String getBrandName();
+	
+	int getAmount();
+	
+	BigDecimal getTotal();
+	
+}
